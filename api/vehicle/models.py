58fd2314c60f2b297e9633c51ed4b69d85cfa644
Leandro Salustriano <leandro.salustriano@hotmail.com>
2023-09-04 14:25:31 -0300

    created vehicle model

--- api/vehicle/models.py
+++ api/vehicle/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Vehicle(models.Model):
+    vehicle_plate = models.CharField(max_length=10)
+    vehicle_brand = models.CharField(max_length=100)
+    vehicle_model = models.CharField(max_length=100)
+    vehicle_oil_change_km = models.IntegerField()
+
+    class Meta:
+        db_table = 'vehicle'
