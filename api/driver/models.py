9a4ea33f5bff17b9ea2b4a7cc7e2fce9fea94908
Leandro Salustriano <leandro.salustriano@hotmail.com>
2023-09-04 14:23:19 -0300

    created driver model

--- api/driver/models.py
+++ api/driver/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Driver(models.Model):
+    driver_name = models.CharField(max_length=200)
+    driver_phone = models.CharField(max_length=16)
+    driver_license = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = 'driver'
