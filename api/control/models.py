569703a69e507aa8eff790cbcf126f2a33451e2b
Leandro Salustriano <leandro.salustriano@hotmail.com>
2023-09-04 14:29:26 -0300

    created control model

--- api/control/models.py
+++ api/control/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from driver.models import Driver
+from vehicle.models import Vehicle
 
-# Create your models here.
+class Control(models.Model):
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+    driver = models.ForeignKey(Driver, on_delete=models.CASCADE)
+    departure_date = models.DateField()
+    departure_time = models.TimeField()
+    departure_km = models.PositiveIntegerField()
+    destination = models.CharField(max_length=200)
+    return_date = models.DateField(null=True, blank=True)
+    return_time = models.TimeField(null=True, blank=True)
+    return_km = models.PositiveIntegerField(null=True, blank=True)
+    distance_traveled = models.PositiveIntegerField(null=True, blank=True)
+    
+    class Meta:
+      db_table = 'control'
