71b82e7eefebc881212a71b8e04625ebee88c7b5
Leandro Salustriano <leandro.salustriano@hotmail.com>
2023-09-05 23:26:44 -0300

    added missing driver to installed apps

--- api/api/settings.py
+++ api/api/settings.py
@@ -36,11 +36,10 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',    
+    'django.contrib.staticfiles',
     'driver',
     'vehicle',
     'control',
-    'drf_yasg'
 ]
 
 MIDDLEWARE = [
