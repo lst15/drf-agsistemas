479f02cbfcf4763c56537ec98140e41155f1dd76
Leandro Salustriano <leandro.salustriano@hotmail.com>
2023-09-07 13:48:50 -0300

    modify url control pattern

--- api/control/urls.py
+++ api/control/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import create,update,delete,fetch,fetchAll,vehicleKmControl
 
 urlpatterns = [
-  path("control",create.as_view(), name="create"),
-  path("control/<int:control_id>",update.as_view(),name="update"),
-  path("control/<int:control_id>/delete",delete.as_view(),name="delete"),
-  path("control/<int:control_id>/fetch",fetch.as_view(),name="fetch"),
+  path("control",create.as_view(), name="create"),  
+  path("control/vehicle/<int:vehicle_id>/km",vehicleKmControl.as_view(),name="get vehicle km control"),
+  path("control/<int:control_id>/update",update.as_view(),name="update"),    
+  path("control/<int:control_id>",fetch.as_view(),name="fetch"),  
   path("control/fetch",fetchAll.as_view(),name="fetchAll"),
-  path("control/vehicle/<int:vehicle_id>/km",vehicleKmControl.as_view(),name="get vehicle km control")
+  path("control/<int:control_id>/delete",delete.as_view(),name="delete"),
 ]
